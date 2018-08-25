94dc101e01a1353803c765573194412f31470192
vagrant <vagrant@localhost.localdomain>
2018-08-25 16:02:59 +0000

    made changes

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -6,8 +6,20 @@ package 'ntp'
 
 package 'git'
 
+#node['ipaddress']
+#node['memory']['total']
+
+# print statement 'I have 4 apples'
+#apple_count = 4
+#puts "I have #{apple_count} apples"
+
 file '/etc/motd' do
-	content 'This server is the property of TechnoTrainer'
+	content "This server is the property of TechnoTrainer
+	HOSTNAME: #{node['hostname']}
+	IPADDRESS: #{node['ipaddress']}
+	CPU #{node['cpu']['0']['mhz']}
+	MEMORY #{node['memory']['total']} 
+	"
 	action :create
 	owner 'root'
         group 'root'
